e67a0b437684826f6f03116bb9d789f7db69e045
Myungin Lee <benedic.lee@gmail.com>
2026-02-17 13:11:29 -0500

    Packages cleaned up

--- Assets/Scripts/Audio/AudioSpectrum.cs
+++ Assets/Scripts/Audio/AudioSpectrum.cs
@@ -3,6 +3,7 @@
 // Author: Myungin Lee
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent (typeof(AudioSource))]
@@ -13,6 +14,7 @@ public class AudioSpectrum : MonoBehaviour
     public static int FFTSIZE = 4096; // https://en.wikipedia.org/wiki/Fast_Fourier_transform
     public static float[] samples = new float[FFTSIZE];
     public static float audioAmp = 0f;
+    float percussion, brass;
     void Start()
     {
         source = GetComponent<AudioSource>();       
@@ -26,6 +28,23 @@ public class AudioSpectrum : MonoBehaviour
         for (int i = 0; i < FFTSIZE; i++)
         {
             audioAmp += samples[i];
-        }        
+        }
+        percussion = 0f;
+        brass = 0f;
+
+        // 2~10 : percussion
+        for (int i = 2; i < 10; i++)
+        {
+            percussion += samples[i];
+        }
+
+        percussion = percussion / 8f;
+
+        // 25~50 : brass
+        for (int i = 25; i < 50; i++)
+        {
+            brass += samples[i];
+        }
+        brass = brass / 25f;
     }
 }
